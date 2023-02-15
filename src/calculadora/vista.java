ae08528f06ea185c553948a13d579c6b0f4d6fd0
Nahuel Ahumada <ahumada.nahuel35149@gmail.com>
2023-02-15 11:28:15 -0300

    Grid Layout insertado

--- src/calculadora/vista.java
+++ src/calculadora/vista.java
@@ -5,6 +5,11 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.GridLayout;
 
 public class vista extends JFrame {
 
@@ -34,6 +39,14 @@ public class vista extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 104, 296, 323);
+		contentPane.add(panel);
+		
+		panel.setLayout(new GridLayout(5, 4, 0, 0));
+		
+		
 	}
-
 }
