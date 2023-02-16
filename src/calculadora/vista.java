a927a331efa12dba59f972fb86c7a1d1ddb0f8f6
Nahuel Ahumada <ahumada.nahuel35149@gmail.com>
2023-02-16 12:56:05 -0300

    Vista añadida con JTatto

--- src/calculadora/vista.java
+++ src/calculadora/vista.java
@@ -13,13 +13,27 @@ import java.awt.GridLayout;
 import java.awt.Font;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
+import javax.swing.UIManager;
 
 public class vista extends JFrame {
-
+	
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		try {
+            //here you can put the selected theme class name in JTattoo
+            UIManager.setLookAndFeel("com.jtattoo.plaf.graphite.GraphiteLookAndFeel");
+ 
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(vista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(vista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(vista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(vista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
